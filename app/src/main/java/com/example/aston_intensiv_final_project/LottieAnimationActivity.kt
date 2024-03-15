3af0e4cad8eeb18978b29e376df07b5a67ae8799
Azamat <azamat.ahmetshin93@gmail.com>
2024-03-15 14:41:07 +0300

    splash screen and lottie added

---
+++ app/src/main/java/com/example/aston_intensiv_final_project/LottieAnimationActivity.kt
@@ -0,0 +1,33 @@
+package com.example.aston_intensiv_final_project
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.airbnb.lottie.LottieAnimationView
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+class LottieAnimationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lottie_animation)
+        splashScreen.setOnExitAnimationListener { vp ->
+            val lottieView = findViewById<LottieAnimationView>(R.id.animationView)
+            lottieView.enableMergePathsForKitKatAndAbove(true)
+
+            lottieView.post {
+                vp.view.alpha = 0f
+                vp.iconView.alpha = 0f
+                lottieView!!.playAnimation()
+            }
+        }
+
+        Timer().schedule(3900) {
+            val intent = Intent(this@LottieAnimationActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
